16a31f3cefa22aa92a084676639dc8d50a7558ed
menglingchao <75912001@qq.com>
2025-03-27 09:48:44 +0800

    [refactor]优化

--- common/service.go
+++ common/service.go
@@ -2,5 +2,6 @@ package common
 
 type ServerNet struct {
 	Addr *string `json:"addr"` // e.g.: 127.0.0.0:8989 [default]: ""
+	Name *string `json:"name"` // 链接名称 [default]: ""
 	Type *string `json:"type"` // [tcp, kcp] [default]: xnetcommon.ServerNetTypeNameTCP
 }
