7d6be4f25d42abd4b1069b9f384bcc597c333ca8
menglingchao <75912001@qq.com>
2026-04-16 15:27:16 +0800

    [refactor] 简化 runtime

--- runtime/code.location.go
+++ runtime/code.location.go
@@ -1,15 +1,13 @@
 package runtime
 
 import (
-	xerror "github.com/75912001/xlib/error"
-	xpool "github.com/75912001/xlib/pool"
-	"github.com/pkg/errors"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strconv"
-	"strings"
-	"sync"
+
+	xerror "github.com/75912001/xlib/error"
+	"github.com/pkg/errors"
 )
 
 var ExecutablePath string // 程序所在路径(如为link,则为link所在的路径)
@@ -20,56 +18,13 @@ func init() {
 	}
 }
 
-// 代码位置
-type codeLocation struct {
-	fileName string //文件名
-	funcName string //函数名
-	line     int    //行数
-}
-
-// String 信息
-func (p *codeLocation) String() string {
-	// 预计算字符串长度，避免 strings.Builder 动态扩容
-	strLine := strconv.Itoa(p.line)
-	size := len("file:") + len(p.fileName) +
-		len(" line:") + len(strLine) +
-		len(" func:") + len(p.funcName)
-
-	b := xpool.Builder.Get()
-	defer func() {
-		xpool.Builder.Put(b)
-	}()
-	b.Grow(size) // 预分配内存空间
-
-	b.WriteString("file:")
-	b.WriteString(p.fileName)
-	b.WriteString(" line:")
-	b.WriteString(strLine)
-	b.WriteString(" func:")
-	b.WriteString(p.funcName)
-	return strings.Clone(b.String())
-}
-
-// Location 使用对象池复用 codeLocation 对象
-var locationPool = sync.Pool{
-	New: func() any {
-		return &codeLocation{
-			fileName: xerror.Unknown.Name(),
-			funcName: xerror.Unknown.Name(),
-		}
-	},
-}
-
 // Location 获取代码位置
 func Location() string {
-	location := locationPool.Get().(*codeLocation)
-	defer locationPool.Put(location)
 	if pc, fileName, line, ok := runtime.Caller(1); ok {
-		location.fileName = fileName
-		location.line = line
-		location.funcName = runtime.FuncForPC(pc).Name()
+		return "file:" + fileName + " line:" + strconv.Itoa(line) + " func:" + runtime.FuncForPC(pc).Name()
 	}
-	return location.String()
+	unknown := xerror.Unknown.Name()
+	return "file:" + unknown + " line:0 func:" + unknown
 }
 
 // GetExecutablePath 获取当前执行的程序-绝对路径,指向启动当前进程的可执行文件-目录路径.
