0777c930790b150f99d413a650007d8f4c55922e
menglingchao <75912001@qq.com>
2026-04-22 18:08:21 +0800

    [refactor] hashring 中 增加判断

--- grpc/proto/interceptor/timeout.go
+++ grpc/proto/interceptor/timeout.go
@@ -2,6 +2,7 @@ package interceptor
 
 import (
 	"context"
+	xgrpcproto "github.com/75912001/xlib/grpc/proto"
 	xgrpcprotoregistry "github.com/75912001/xlib/grpc/proto/registry"
 	"google.golang.org/grpc"
 	"time"
@@ -11,8 +12,11 @@ func TimeOutClientInterceptor() grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		// 获取超时时间
 		opt := xgrpcprotoregistry.GetOptions(method)
-		// 创建带超时的 context
-		duration, _ := time.ParseDuration(opt.Timeout)
+		duration, err := time.ParseDuration(opt.Timeout)
+		if err != nil || duration <= 0 {
+			// 非法或空配置时避免 WithTimeout(..., 0) 导致立刻失败，与 GetOptions 默认语义对齐
+			duration = xgrpcproto.RpcTimeoutDurationDefault
+		}
 		ctx, cancel := context.WithTimeout(ctx, duration)
 		defer func() {
 			cancel()
