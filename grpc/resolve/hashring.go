2c7609fd4c909872b774d0e5ed7a4a0eea957c64
menglingchao <75912001@qq.com>
2026-04-22 17:41:32 +0800

    [refactor] hashring 中 增加判断

--- grpc/resolve/hashring.go
+++ grpc/resolve/hashring.go
@@ -1,6 +1,8 @@
 package resolve
 
 import (
+	"sync"
+
 	xgrpcutil "github.com/75912001/xlib/grpc/util"
 	xhashring "github.com/75912001/xlib/hashring"
 	xmap "github.com/75912001/xlib/map"
@@ -9,40 +11,48 @@ import (
 var gHashRingMgr = newHashRingMgr()
 
 type hashRing struct {
-	*xmap.MapMgr[string, *xhashring.HashRing[string]] // key: genPackageNameServiceName() val: 哈希环(node:ServerKey.String())
+	// writer 串行化 add/del 中的「Find → 构造新环 → Add」，避免仅替换 Map 时并发丢更新
+	writer sync.Mutex
+	m      *xmap.MapMutexMgr[string, *xhashring.HashRing[string]] // key: genPackageNameServiceName() val: 哈希环(node:ServerKey.String())
 }
 
 func newHashRingMgr() *hashRing {
 	return &hashRing{
-		MapMgr: xmap.NewMapMgr[string, *xhashring.HashRing[string]](),
+		m: xmap.NewMapMutexMgr[string, *xhashring.HashRing[string]](),
 	}
 }
 
 func (p *hashRing) add(packageName string, serviceName string, node string) {
 	packageNameServiceName := genPackageNameServiceName(packageName, serviceName)
-	oldHashRing, ok := p.MapMgr.Find(packageNameServiceName)
-	if !ok { // 如果不存在，则创建一个新的哈希环
+	p.writer.Lock()
+	defer p.writer.Unlock()
+
+	oldHashRing, ok := p.m.Find(packageNameServiceName)
+	if !ok {
 		newHashRing := xhashring.NewHashRing[string]().AddNode(node)
-		p.MapMgr.Add(packageNameServiceName, newHashRing)
-	} else { // 如果存在，则更新哈希环
-		newHashRing := oldHashRing.AddNode(node)
-		p.MapMgr.Add(packageNameServiceName, newHashRing)
+		p.m.Add(packageNameServiceName, newHashRing)
+		return
 	}
+	newHashRing := oldHashRing.AddNode(node)
+	p.m.Add(packageNameServiceName, newHashRing)
 }
 
 func (p *hashRing) del(packageName string, serviceName string, node string) {
 	packageNameServiceName := genPackageNameServiceName(packageName, serviceName)
-	v, ok := p.MapMgr.Find(packageNameServiceName)
+	p.writer.Lock()
+	defer p.writer.Unlock()
+
+	v, ok := p.m.Find(packageNameServiceName)
 	if !ok {
 		return
 	}
 	newHashRing := v.RemoveNode(node)
-	p.MapMgr.Add(packageNameServiceName, newHashRing)
+	p.m.Add(packageNameServiceName, newHashRing)
 }
 
 func (p *hashRing) get(packageName string, serviceName string, shareKey string) xgrpcutil.IClientConn {
 	packageNameServiceName := genPackageNameServiceName(packageName, serviceName)
-	v, ok := p.MapMgr.Find(packageNameServiceName)
+	v, ok := p.m.Find(packageNameServiceName)
 	if !ok {
 		return nil
 	}
