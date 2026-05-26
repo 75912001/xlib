07d49e5fe9d81fc0add28ed25ab556e7df557c66
lingchao <75912001@qq.com>
2026-05-26 20:09:24 +0800

    1

--- config/config.redis.go
+++ config/config.redis.go
@@ -7,9 +7,12 @@ import (
 )
 
 type Redis struct {
-	Name     *string  `yaml:"name"`     // 名称		[default]: "redisName"
-	Addrs    []string `yaml:"addrs"`    // 地址
-	Password *string  `yaml:"password"` // 密码		[default]:"123456"
+	Name           *string  `yaml:"name"`           // 名称		[default]: "redisName"
+	Addrs          []string `yaml:"addrs"`          // 地址
+	Password       *string  `yaml:"password"`       // 密码		[default]:"123456"
+	DialTimeoutMS  *int     `yaml:"dialTimeoutMS"`  // 连接超时时间-毫秒	[default]: 3000
+	ReadTimeoutMS  *int     `yaml:"readTimeoutMS"`  // 读超时时间-毫秒	[default]: 3000
+	WriteTimeoutMS *int     `yaml:"writeTimeoutMS"` // 写超时时间-毫秒	[default]: 3000
 }
 
 func (p *Redis) Configure() error {
@@ -23,6 +26,18 @@ func (p *Redis) Configure() error {
 	if p.Password == nil {
 		defaultValue := "123456"
 		p.Password = &defaultValue
+	}
+	if p.DialTimeoutMS == nil {
+		defaultValue := 3000
+		p.DialTimeoutMS = &defaultValue
+	}
+	if p.ReadTimeoutMS == nil {
+		defaultValue := 3000
+		p.ReadTimeoutMS = &defaultValue
+	}
+	if p.WriteTimeoutMS == nil {
+		defaultValue := 3000
+		p.WriteTimeoutMS = &defaultValue
 	}
 	return nil
 }
